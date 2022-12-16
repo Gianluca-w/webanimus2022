4bbcb0a039ecb1f21e2de737df88f9b956379fa6
Gianluca <barreyro.gian@gmail.com>
2022-12-16 12:58:10 -0300

    fixed resume form, refactored for it to work better

--- components/ContactSubmitResume.js
+++ components/ContactSubmitResume.js
@@ -5,13 +5,13 @@ const ContactSubmitResume = ({ }) => {
     const [mail, setMail] = useState("");
     const [phone, setPhone] = useState("");
     const [message, setMessage] = useState("");
-    function SanitizeGeneral(ToSanitize,ReturnTo,MaxLength=580,Desinfectant=/[/[\]{}"'$]/g){
-        ToSanitize.value=ToSanitize.value.slice(0,MaxLength)
-        const SanitizedValue = ToSanitize.value.replaceAll(Desinfectant,"")
-        if (ReturnTo=="Name") {setName(SanitizedValue);}
-        if (ReturnTo=="Mail") {setMail(SanitizedValue);}
-        if (ReturnTo=="Phone") {ToSanitize.value=SanitizedValue; setPhone(SanitizedValue);}
-        if (ReturnTo=="Message") {setMessage(SanitizedValue);}
+    function SanitizeGeneral(ToSanitize, ReturnTo, MaxLength = 580, Desinfectant = /[/[\]{}"'$]/g) {
+        ToSanitize.value = ToSanitize.value.slice(0, MaxLength)
+        const SanitizedValue = ToSanitize.value.replaceAll(Desinfectant, "")
+        if (ReturnTo == "Name") { setName(SanitizedValue); }
+        if (ReturnTo == "Mail") { setMail(SanitizedValue); }
+        if (ReturnTo == "Phone") { ToSanitize.value = SanitizedValue; setPhone(SanitizedValue); }
+        if (ReturnTo == "Message") { setMessage(SanitizedValue); }
     }
 
     return <div className={`ContactFormWrapper`}>
@@ -19,48 +19,33 @@ const ContactSubmitResume = ({ }) => {
             <div className='ContactFormInputWrapper'>
                 <div className='ContactFormWideInput'>
                     <p>Name</p>
-                    <input type="text" placeholder='Name' onChange={(event) => SanitizeGeneral(event.target,"Name",30)}></input>
+                    <input type="text" placeholder='Name' onChange={(event) => SanitizeGeneral(event.target, "Name", 30)}></input>
                 </div>
                 <div className='ContactFormWideInput'>
                     <p>Email</p>
-                    <input type="mail" placeholder='Email' onChange={(event) => SanitizeGeneral(event.target, "Mail",35)}></input>
+                    <input type="mail" placeholder='Email' onChange={(event) => SanitizeGeneral(event.target, "Mail", 35)}></input>
                 </div>
                 <div className='ContactFormPhoneInput'>
                     <p>Phone</p>
-                    <input type="text" placeholder='Phone' onChange={(event) => SanitizeGeneral(event.target, "Phone",15,/[^0-9+]/g)}></input>
+                    <input type="text" placeholder='Phone' onChange={(event) => SanitizeGeneral(event.target, "Phone", 15, /[^0-9+]/g)}></input>
                 </div>
-            </div>
-        </div>
-
-        <div>
-            <div className='ContactFormInputWrapper'>
-                <div className='ContactFormWideInput'>
-                    <p>Message</p>
-                </div>
-                <div className='ContactFormRestricted40Input'>
-                    <p>Resume</p>
-                </div>
-
             </div>
         </div>
 
         <div className='ContactFormInputWrapper'>
-            <div className='ContactFormUnderlining'>
-                <div className='ContactFormWideInput'>
-                    <textarea type='text' placeholder='Message' className='' onChange={(event)=>SanitizeGeneral(event.target,"Message")}>
-                    </textarea>
-                </div>
+            <div className='ContactFormWideInput'>
+                <p>Message</p>
+                <textarea type='text' placeholder='Your message' name='message' className='' onChange={(event) => SanitizeGeneral(event.target, "Message")}>
+
+                </textarea>
             </div>
-            <div className='ContactFormRestricted40Input'>
-                <div className='DragAndDropBorder'>
-                    DRAG AND DROP HERE
-                </div>
-
+            <div className='ContactFormWideInput'>
+                
+                <div><p style={{paddingLeft:"10px"}}>Resume</p></div>
+                <div className="DragAndDropWrapper"><div className="DragAndDropArea"> Drag and Drop here</div></div>
+                
             </div>
-
-
         </div>
-
 
     </div>
 }
