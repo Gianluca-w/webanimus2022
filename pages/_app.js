3bb4a99214561edc4a56e1bf78b2f629683966ac
Gianluca-W <barreyro.gian@gmail.com>
2023-02-03 15:17:07 -0300

    reverted splashscreen

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,9 @@
 import '../styles/general.scss';
 import { appWithTranslation } from 'next-i18next'
 import nextI18nextConfig from '../next-i18next.config';
-import React, { useEffect } from 'react';
+
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const loader = document.getElementById('splashScreen');
-      if (loader)
-        loader.classList.add("loaded");
-    }
-  }, []);
   return <Component {...pageProps} />
 }
 
