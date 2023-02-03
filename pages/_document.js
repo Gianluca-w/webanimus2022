4027e7793efeb5e110915b65501b21bf062385ae
Gianluca-W <barreyro.gian@gmail.com>
2023-02-03 11:06:59 -0300

    Preloading fonts

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,14 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=10" /></Head>
+                <meta name="viewport" content="width=device-width, initial-scale=10" />
+                <link
+                    rel="preload"
+                    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700&display=swap"
+                    as="font"
+                    crossOrigin=""
+                    type="font/woff2"
+                /></Head>
             <body>
                 <Main />
                 <NextScript />
